682a6bb090bdf8cc3e0fe0e1de66780ce441b88b
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-10 17:02:22 -0600

    added login/reg controllers and token service. Not yet included in html.

--- AngularSite/Scripts/AngularWebApp.js
+++ AngularSite/Scripts/AngularWebApp.js
@@ -1,2 +1,5 @@
 var AngularWebApp = angular.module('AngularWebApp', []);
 AngularWebApp.controller('BaseController', BaseController);
+AngularWebApp.service('SessionService', SessionService);
+AngularWebApp.factory('LoginFactory', LoginFactory);
+AngularWebApp.factory('RegisterFactory', RegisterFactory);
---
+++ AngularSite/Scripts/Services/SessionService.js
@@ -0,0 +1,3 @@
+var AuthenticationService = function(){
+    this.token = undefined;
+}
